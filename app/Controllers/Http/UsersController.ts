f4da303f0f45ce5eba17ab866c0987d6d61a8dc8
Lucas Andrade <lucasantos.dev@gmail.com>
2022-08-14 23:16:55 -0300

    store user in redis

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CreateUserValidator from 'App/Validators/CreateUserValidator'
+import Redis from '@ioc:Adonis/Addons/Redis'
 import User from 'App/Models/User'
 
 export default class UsersController {
@@ -12,7 +13,8 @@ export default class UsersController {
       return response.unprocessableEntity({ message: 'User already registered' })
     }
 
-    const { id, name, email } = await User.create(payload)
+    const { id, name, email, password } = await User.create(payload)
+    await Redis.set(`user-${id}`, JSON.stringify({ id, name, email, password }))
 
     return response.created({
       message: 'User created',
