d19b6c5cb7f1c50898b68febebe53b6c0bcd6905
Lucas Andrade <lucasantos.dev@gmail.com>
2022-08-16 14:26:12 -0300

    create auth token when creating user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -4,7 +4,7 @@ import Redis from '@ioc:Adonis/Addons/Redis'
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async store({ request, response }: HttpContextContract) {
+  public async store({ request, response, auth }: HttpContextContract) {
     const payload = await request.validate(CreateUserValidator)
 
     const user = await User.findBy('email', payload.email)
@@ -13,12 +13,18 @@ export default class UsersController {
       return response.unprocessableEntity({ message: 'User already registered' })
     }
 
-    const { id, name, email, password } = await User.create(payload)
+    const newUser = await User.create(payload)
+    const { id, name, email, password } = newUser
+
     await Redis.set(`user-${id}`, JSON.stringify({ id, name, email, password }))
+    const token = await auth.use('api').generate(newUser, {
+      expiresIn: '7days',
+    })
 
     return response.created({
       message: 'User created',
       data: { id, name, email },
+      token,
     })
   }
 }
